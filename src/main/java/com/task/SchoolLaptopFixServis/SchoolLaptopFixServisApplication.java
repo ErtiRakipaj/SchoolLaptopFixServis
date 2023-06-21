d09251eb06df5da88616e606246cbc393d592942
ErtiDesu <erti.rakipaj@gmail.com>
2023-06-21 13:10:50 +0200

    Initial Commit

---
+++ src/main/java/com/task/SchoolLaptopFixServis/SchoolLaptopFixServisApplication.java
@@ -0,0 +1,13 @@
+package com.task.SchoolLaptopFixServis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolLaptopFixServisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolLaptopFixServisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/SchoolLaptopFixServis/SchoolLaptopFixServisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.SchoolLaptopFixServis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolLaptopFixServisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
